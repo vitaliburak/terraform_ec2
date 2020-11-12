745085b0a57a3a4dc1cab39e5fbfadd42b2857ae
Vitalii Burak <vvitaliy972@gmail.com>
2020-11-12 17:29:45 +0000

    ec2_ubuntu

---
+++ ec2_instance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "Ubuntu" {
+  ami                         = "ami-0739f8cdb239fe9ae"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = "true"
+  key_name                    = "${aws_key_pair.bastion1.key_name}"
+  user_data                   = "${file("userdata.sh")}"
+  vpc_security_group_ids      = ["${aws_security_group.winter.id}"]
+  availability_zone           = "us-east-1a"
+
+  tags = {
+    Name        = "Ubuntu"
+    Environment = "Dev"
+  }
+}
